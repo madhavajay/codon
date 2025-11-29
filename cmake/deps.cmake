95f97300b8eee4e02b98d3b0d4ad56b510d24d82
Madhava Jay <me@madhavajay.com>
2025-11-30 07:25:06 +1000

    linux fixes

--- cmake/deps.cmake
+++ cmake/deps.cmake
@@ -1,8 +1,14 @@
 set(CPM_DOWNLOAD_VERSION 0.32.3)
 set(CPM_DOWNLOAD_LOCATION "${CMAKE_BINARY_DIR}/cmake/CPM_${CPM_DOWNLOAD_VERSION}.cmake")
 if(NOT (EXISTS ${CPM_DOWNLOAD_LOCATION}))
-    message(STATUS "Downloading CPM.cmake...")
-    file(DOWNLOAD https://github.com/TheLartians/CPM.cmake/releases/download/v${CPM_DOWNLOAD_VERSION}/CPM.cmake ${CPM_DOWNLOAD_LOCATION})
+    # Try bundled CPM.cmake (checked in) to avoid network fetches.
+    if(EXISTS "${CMAKE_SOURCE_DIR}/cmake/CPM.cmake")
+        file(COPY "${CMAKE_SOURCE_DIR}/cmake/CPM.cmake" DESTINATION "${CMAKE_BINARY_DIR}/cmake/")
+        file(RENAME "${CMAKE_BINARY_DIR}/cmake/CPM.cmake" "${CPM_DOWNLOAD_LOCATION}")
+    else()
+        message(STATUS "Downloading CPM.cmake...")
+        file(DOWNLOAD https://github.com/TheLartians/CPM.cmake/releases/download/v${CPM_DOWNLOAD_VERSION}/CPM.cmake ${CPM_DOWNLOAD_LOCATION})
+    endif()
 endif()
 include(${CPM_DOWNLOAD_LOCATION})
 
